510a020e774c696da5970c09f0191987ca1eafb3
Jocimar Walter <f5078775@bb.com.br>
2024-05-20 08:53:24 -0300

    atualiado 2024-05-20

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,6 +6,31 @@ const jwt = require("jsonwebtoken");
 const {verifyToken} = require("../utils/verifyToken.js");
 
 
+
+//GERAR TOKEN DE TESTE
+
+
+router.get("/gerartoken", (req, res) => {
+  console.log("Test successful");
+  console.log("Headers received", req.headers);
+
+  const token = jwt.sign(
+    { userId: "123", teste: "batata 2", data_criacao_utc: new Date() },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  // Adicionando o token JWT como um cookie na resposta
+  res.cookie("jwtToken", token, {
+    maxAge: 3600000, // Tempo de vida do cookie em milissegundos (1 hora)
+    httpOnly: true, // Define se o cookie é acessível apenas pelo servidor
+  });
+
+  res.status(200).json({ result: "ok" });
+});
+
+
+
 //REGISTER USER
 
 router.post("/register", async (req, res) => {
--- src/routes/user.js
+++ src/routes/user.js
@@ -9,24 +9,7 @@ const {
 } = require("../utils/verifyToken");
 const User = require("../models/User.js");
 
-router.get("/gerartoken", (req, res) => {
-  console.log("Test successful");
-  console.log("Headers received", req.headers);
 
-  const token = jwt.sign(
-    { userId: "123", teste: "batata 2", data_criacao_utc: new Date() },
-    process.env.JWT_SECRET,
-    { expiresIn: "1h" }
-  );
-
-  // Adicionando o token JWT como um cookie na resposta
-  res.cookie("jwtToken", token, {
-    maxAge: 3600000, // Tempo de vida do cookie em milissegundos (1 hora)
-    httpOnly: true, // Define se o cookie é acessível apenas pelo servidor
-  });
-
-  res.status(200).json({ result: "ok" });
-});
 
 router.get("/test", (req, res) => {
   // Extrair o token do cookie
